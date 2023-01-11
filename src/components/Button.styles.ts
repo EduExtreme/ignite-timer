f335eeca7c3380da2a1e4790bad97fd7408e6df9
Eduardo André <edugexp@gmail.com>
2023-01-11 09:27:35 -0400

    eslint config before prettier

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -1,23 +1,23 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
-export type ButtonVariant = 'primary' | 'secondary' | 'danger' | "success";
+export type ButtonVariant = 'primary' | 'secondary' | 'danger' | 'success'
 
 interface ButtonContainerProps {
-  variant: ButtonVariant;
+  variant: ButtonVariant
 }
 const buttonVariants = {
   primary: 'purple',
-  secondary:'orange',
-  danger:'red',
-  success: 'green'
+  secondary: 'orange',
+  danger: 'red',
+  success: 'green',
 }
 export const ButtonContainer = styled.button<ButtonContainerProps>`
-width: 100px;
-height: 70px;
+  width: 100px;
+  height: 70px;
 
-${ props => {
-  return `background-color: ${buttonVariants[props.variant]}`
-}}
+  background-color: ${(props) => props.theme['green-500']};
 
-
-`;
+  /* ${(props) => {
+    return `background-color: ${buttonVariants[props.variant]}`
+  }} */
+`
